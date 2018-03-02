9515693cc59da44eb98d4e935b2f9a774ea0914e
Murry Jeong <comchangs@gmail.com>
2018-03-03 00:11:49 +0900

    angular app이 컴파일되어 내부에 저장된 static html page를 webview로 보여주는 example

---
+++ app/src/main/java/net/jwnc/example/angularapp/MainActivity.java
@@ -0,0 +1,27 @@
+package net.jwnc.example.angularapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.loadUrl("file:///android_asset/index.html");
+    }
+
+}
